78d9ffe7e37ede7aa4611b8b5e18bb13fe805a2c
paulg <paul@pgoergen.de>
2014-08-01 21:12:29 +0200

    added exception handling

--- dashcam/distanceMeter.py
+++ dashcam/distanceMeter.py
@@ -28,15 +28,18 @@ class distanceMeter(threading.Thread):
         print "starting loop"
         while self.running:
             # grab EACH set of gpsd info to clear the buffer
-            rawdata=self.distanceProcess.stdout.read(4)
-            tmp=numpy.fromstring(rawdata,dtype="<f4")[-1]
-            if (numpy.abs(self.lastFive-tmp)<5).any():
-          #      print self.lastFive
-                self.currentdistance=tmp
-                self.lastFiveValid[self.currentValid%5]=tmp
-                self.currentValid+=1
-            self.lastFive[self.current%5]=tmp
-            self.current+=1
+            try:
+                rawdata=self.distanceProcess.stdout.read(4)
+                tmp=numpy.fromstring(rawdata,dtype="<f4")[-1]
+                if (numpy.abs(self.lastFive-tmp)<5).any():
+                    #      print self.lastFive
+                    self.currentdistance=tmp
+                    self.lastFiveValid[self.currentValid%5]=tmp
+                    self.currentValid+=1
+                self.lastFive[self.current%5]=tmp
+                self.current+=1
+            except IndexError:
+                print "strange, distance data coming in too slow?"
     def stopController(self):
         self.running = False
         self.distanceProcess.send_signal(2)
